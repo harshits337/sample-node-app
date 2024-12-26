4120857a0d71c7d0a7087a26e455b2e14150a168
Harshit <harshit.sharma@inmobi.com>
2024-12-26 23:46:12 +0530

    Initialize setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+})
+
+app.get('/healthy', (req, res) => {
+    res.status(200).send('healthy');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
